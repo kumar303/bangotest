7b4a247d64feea7578061164e3b851fb4ce1e7eb
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-09-28 10:01:29 -0500

    Adds identity request/response

--- bangotest/payflow/urls.py
+++ bangotest/payflow/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = patterns('',
     url(r'^$', views.home, name='payflow.home'),
     url(r'^process$', views.process, name='payflow.process'),
+    url(r'^identity$', views.identity, name='payflow.identify'),
 )
--- bangotest/payflow/views.py
+++ bangotest/payflow/views.py
@@ -16,14 +16,29 @@ def home(request):
 
 
 def process(request):
+    """Process a payment response redirect."""
     log.info(pprint.pformat(dict(request.REQUEST.items())))
     qs = request.META['QUERY_STRING']
     res = requests.get('https://xml.bango.net/tokenCheck.aspx?%s' % qs)
     tr = etree.fromstring(res.content)
+    # This verifies that the request actually came from Bango.
     valid = tr.attrib['success'] == 'true'
     log.info('valid? %s' % valid)
     if not valid:
         log.error('response: %s' % res.content)
     return render(request, 'payflow/process.html',
+                  {'valid': valid,
+                   'request': request.REQUEST})
+
+
+def identity(request):
+    """Process an identity response redirect."""
+    log.info(pprint.pformat(dict(request.REQUEST.items())))
+    token = request.REQUEST.get('p')
+    # This is where we could verify our own signature
+    # to make sure the identity request originated from us.
+    valid = token == 'ourtoken'
+    log.info('valid? %s' % valid)
+    return render(request, 'payflow/identity.html',
                   {'valid': valid,
                    'request': request.REQUEST})
